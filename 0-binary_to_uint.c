8f851eee4967f8ac3511409f84d173e79e353f8e
nour eddine ichchou <ichchoun@gmail.com>
2023-10-26 05:02:40 -0700

    shiiip

--- 0-binary_to_uint.c
+++ 0-binary_to_uint.c
@@ -1,25 +1,25 @@
 #include "main.h"
-#include <stddef.h>
+
 /**
- * binary_to_uint - Convert a binary number to an unsigned int
- * @b: Pointer to a string of '0' and '1' characters
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * Return: The converted number, or 0 if there is an invalid character or if b is NULL
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-    if (b == NULL)
-        return 0;
+	if (!b)
+		return (0);
 
-    for (; *b != '\0'; b++)
-    {
-        if (*b != '0' && *b != '1')
-            return 0;
-        
-        result = result * 2 + (*b - '0');
-    }
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
 
-    return result;
+	return (dec_val);
 }
