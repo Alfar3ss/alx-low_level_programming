1895fec8c610f7ff79c6f9d54602324d7f774012
nour eddine ichchou <ichchoun@gmail.com>
2023-10-26 05:15:32 -0700

    tffo

--- 0-binary_to_uint.c
+++ 0-binary_to_uint.c
@@ -1,37 +1,26 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
- * binary_to_uint - converts a binary number to an
- * unsigned int.
- * @b: binary.
+ * binary_to_uint - Convert a binary number to an unsigned int
+ * @b: Pointer to a string of '0' and '1' characters
  *
- * Return: unsigned int.
+ * Return: The converted number, or 0 if there is an invalid character or if b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+    unsigned int result = 0;
 
-	if (!b)
-		return (0);
+    if (b == NULL)
+        return 0;
 
-	ui = 0;
+    for (; *b != '\0'; b++)
+    {
+        if (*b != '0' && *b != '1')
+            return 0;
+        
+        result = result * 2 + (*b - '0');
+    }
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-	{
-		if (b[len] != '0' && b[len] != '1')
-		{
-			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
-	}
-
-	return (ui);
+    return result;
 }
